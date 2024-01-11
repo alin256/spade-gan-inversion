437008e2313531681f93f6180ca6c17a4fe9dda3
Sergey Alyaev <saly@norceresearch.no>
2024-01-11 17:01:51 +0100

    updated PET installation script

---
+++ Dependencies/install-pet.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##installing pip for python if not already 
+sudo upt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-venv
+python3 -m pip install --upgrade pip setuptools
+cd ../..
+##cloning the PET repository
+git clone git@github.com:Python-Ensemble-Toolbox/PET.git PET
+##create a PET virtual enviornmnet in venv-PET folder
+python3 -m venv venv-PET
+##acrivate the envirnment
+source venv-PET/bin/activate
+##some pre-requisites that do not work out of the box
+##python3 -m pip install --upgrade pip setuptools
+python3 -m pip install wheel
+python3 setup.py bdist_wheel
+##install PET in the newly created enviornment ("PET" is the relative path to the PET folder)
+python3 -m pip install -e PET
+
