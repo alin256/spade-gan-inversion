e914fad0377508431c8e167d6dc1ad1371cfc422
Sergey Alyaev <saly@norceresearch.no>
2024-01-12 21:40:39 +0100

    added run scripts

---
+++ HardData/run-example.sh
@@ -0,0 +1,3 @@
+source ../../venv-PET/bin/activate
+python run_script.py
+python ../Plotting/plotting.py ../HardData
---
+++ SoftData2/run_example.sh
@@ -0,0 +1,6 @@
+cd data
+python setup.py
+cd ..
+cp ../HardData/posterior_state_estimate.npz prior.npz
+python run_script.py
+python ../Plotting/plotting.py ../SoftData2
